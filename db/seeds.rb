8a3f682be2e53f70f3b8ec6688b23910f15d4634
Italo-Guedes <italotreinamentos@gmail.com>
2023-12-15 15:30:02 -0300

    Estudo Associations

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# d1 = Doctor.create(name: "Estranho 2")
+# d2 = Doctor.create(name: "Banner 2")
+
+# p1 = Patient.create(name: "João")
+# p2 = Patient.create(name: "Maria")
+
+# Appointment.create(doctor: d1, patient: p1, date: Date.today)
+# Appointment.create(doctor: d2, patient: p2, date: Date.today)
+# Appointment.create(doctor: d1, patient: p2, date: Date.today)
+# Appointment.create(doctor: d2, patient: p1, date: Date.today)
+
+# s1 = Student.create(name: "Noob")
+# t1 = Teacher.create(name: "Diego")
+
+c1 = Customer.create(name: "Noob")
+e1 = Employee.create(name: "Diego")
